f5190ed55dede219b94d01b2a0ffb1e582316615
16holson <hunterolson11@yahoo.co.uk>
2024-03-28 09:08:20 -0600

    Able to map

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -5,9 +5,9 @@ from ament_index_python.packages import get_package_share_directory
 
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, ExecuteProcess, TimerAction, RegisterEventHandler
+from launch.actions import IncludeLaunchDescription, ExecuteProcess, TimerAction, RegisterEventHandler, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import Command
+from launch.substitutions import Command, LaunchConfiguration
 from launch.event_handlers import OnProcessStart
 
 from launch_ros.actions import Node
@@ -38,7 +38,35 @@ def generate_launch_description():
         parameters=[{'robot_description': robot_description}, controller_params_file],
     )
 
-    delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
+    scanner_arg = DeclareLaunchArgument(
+        name='scanner',
+        default_value='laser',
+        description='Namespace for sample topics'
+    )
+
+    pointcloud_to_laserscan = Node(
+        package='pointcloud_to_laserscan',
+        executable='pointcloud_to_laserscan_node',
+        remappings=[('cloud_in', [LaunchConfiguration(variable_name='scanner'), '/pointcloud']),
+                    ('scan', [LaunchConfiguration(variable_name='scanner'), '/test'])],
+        parameters=[{
+            'target_frame': 'laser_frame',
+            'transform_tolerance': 0.01,
+            'min_height': 0.0,
+            'max_height': 1.0,
+            'angle_min': -2.79,  # -M_PI/2
+            'angle_max': 2.79,  # M_PI/2
+            'angle_increment': 0.0087,  # M_PI/360.0
+            'scan_time': 0.3333,
+            'range_min': 0.0,
+            'range_max': 4.0,
+            'use_inf': True,
+            'inf_epsilon': 1.0
+        }],
+        name='pointcloud_to_laserscan'
+    )
+
+    delayed_controller_manager = TimerAction(period=1.5, actions=[controller_manager])
     
     diff_drive_spawner = Node(
         package="controller_manager",
@@ -69,8 +97,10 @@ def generate_launch_description():
 
     # Launch them all!
     return LaunchDescription([
+        scanner_arg,
         rsp,
         delayed_controller_manager,
+        pointcloud_to_laserscan,
         delayed_diff_drive_spawner,
         delayed_joint_broad_spawner,
     ])
