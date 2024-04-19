74dbf70f68bdd503771b7c1564b89c963d562910
16holson <hunterolson11@yahoo.co.uk>
2024-04-19 09:34:31 -0600

    New body design

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -45,6 +45,10 @@ def generate_launch_description():
         )
     ])
 
+    #lidar_flip = IncludeLaunchDescription(
+        #PythonLaunchDescriptionSource([get_package_share_directory("lidar_flip"), "/launch/lidar_flip.launch.py"])
+    #)
+
     twist_mux_params = os.path.join(get_package_share_directory(package_name), 'config', 'twist_mux.yaml')
     twist_mux = Node(
         package="twist_mux",
@@ -63,34 +67,41 @@ def generate_launch_description():
         parameters=[{'robot_description': robot_description}, controller_params_file],
     )
 
-    scanner_arg = DeclareLaunchArgument(
-        name='scanner',
-        default_value='laser',
-        description='Namespace for sample topics'
-    )
+    #qos_reliability_arg = DeclareLaunchArgument(
+     #   name='qos_reliability',
+     #   default_value='reliable',
+     #   description='QoS reliability setting for pointcloud_to_laserscan node'
+    #)
 
-    pointcloud_to_laserscan = Node(
-        package='pointcloud_to_laserscan',
-        executable='pointcloud_to_laserscan_node',
-        remappings=[('cloud_in', [LaunchConfiguration(variable_name='scanner'), '/pointcloud']),
-                    ('scan', '/scan')],
+    #scanner_arg = DeclareLaunchArgument(
+       # name='scanner',
+      #  default_value='laser',
+      #  description='Namespace for sample topics'
+    #)
+
+    #pointcloud_to_laserscan = Node(
+     #   package='pointcloud_to_laserscan',
+      #  executable='pointcloud_to_laserscan_node',
+       # remappings=[('cloud_in', [LaunchConfiguration(variable_name='scanner'), '/pointcloud']),
+        #            ('scan', '/scan')],
                     # ('scan', [LaunchConfiguration(variable_name='scanner'), '/test'])],
-        parameters=[{
-            'target_frame': 'laser_frame',
-            'transform_tolerance': 0.01,
-            'min_height': 0.0,
-            'max_height': 1.0,
-            'angle_min': -2.79,  # -M_PI/2
-            'angle_max': 2.79,  # M_PI/2
-            'angle_increment': 0.0087,  # M_PI/360.0
-            'scan_time': 0.3333,
-            'range_min': 0.0,
-            'range_max': 50.0,
-            'use_inf': True,
-            'inf_epsilon': 1.0
-        }],
-        name='pointcloud_to_laserscan'
-    )
+        #parameters=[{
+         #   'target_frame': 'laser_frame',
+          #  'transform_tolerance': 0.01,
+           # 'min_height': 0.0,
+           # 'max_height': 1.0,
+           # 'angle_min': -2.79,  # -M_PI/2
+           # 'angle_max': 2.79,  # M_PI/2
+           # 'angle_increment': 0.0087,  # M_PI/360.0
+           # 'scan_time': 0.3333,
+           # 'range_min': 0.0,
+          #  'range_max': 50.0,
+         #   'use_inf': True,
+        #    'inf_epsilon': 1.0,
+       #     'qos_overrides./parameter_events.publisher.reliability': LaunchConfiguration(variable_name='qos_reliability')
+      #  }],
+     #   name='pointcloud_to_laserscan',
+    #)
 
     delayed_controller_manager = TimerAction(period=1.5, actions=[controller_manager])
     
@@ -126,10 +137,12 @@ def generate_launch_description():
         rsp,
         joystick,
         twist_mux,
+        #qos_reliability_arg,
         #lidar,
+        #lidar_flip,
         delayed_controller_manager,
-        scanner_arg,
-        pointcloud_to_laserscan,
+        #scanner_arg,
+        #pointcloud_to_laserscan,
         delayed_diff_drive_spawner,
         delayed_joint_broad_spawner,
     ])
